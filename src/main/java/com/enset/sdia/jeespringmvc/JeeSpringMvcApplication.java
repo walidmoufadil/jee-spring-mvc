0aafd82d4f68ac674fcd54835d7171d8daf38d2e
walidmoufadil <walid.moufadil-etu@etu.univh2c.ma>
2025-03-17 13:25:54 +0000

    initial commit

---
+++ src/main/java/com/enset/sdia/jeespringmvc/JeeSpringMvcApplication.java
@@ -0,0 +1,13 @@
+package com.enset.sdia.jeespringmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeeSpringMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JeeSpringMvcApplication.class, args);
+    }
+
+}
