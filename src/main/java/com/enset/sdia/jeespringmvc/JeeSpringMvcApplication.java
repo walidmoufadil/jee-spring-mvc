880fb2838e9721bf9ca6737cc9ca13252b14d472
walidmoufadil <walid.moufadil-etu@etu.univh2c.ma>
2025-03-17 16:18:03 +0000

    add test method in the main application

--- src/main/java/com/enset/sdia/jeespringmvc/JeeSpringMvcApplication.java
+++ src/main/java/com/enset/sdia/jeespringmvc/JeeSpringMvcApplication.java
@@ -1,13 +1,106 @@
 package com.enset.sdia.jeespringmvc;
 
+import com.enset.sdia.jeespringmvc.entity.Patient;
+import com.enset.sdia.jeespringmvc.repository.PatientRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Date;
+
 @SpringBootApplication
-public class JeeSpringMvcApplication {
+public class JeeSpringMvcApplication implements CommandLineRunner {
+    @Autowired
+    private PatientRepo patientRepo;
 
     public static void main(String[] args) {
         SpringApplication.run(JeeSpringMvcApplication.class, args);
     }
 
+
+    @Override
+    public void run(String... args) throws Exception {
+        patientRepo.save(
+                Patient.builder()
+                        .nom("Doe")
+                        .malade(Math.random()>0.5)
+                        .score((int) Math.random() * 100)
+                        .dateNaissance(new Date())
+                        .build()
+        );
+        patientRepo.save(
+                Patient.builder()
+                        .nom("Cartner")
+                        .malade(Math.random()>0.5)
+                        .score((int) Math.random() * 100)
+                        .dateNaissance(new Date())
+                        .build()
+        );
+        patientRepo.save(
+                Patient.builder()
+                        .nom("John")
+                        .malade(Math.random()>0.5)
+                        .score((int) Math.random() * 100)
+                        .dateNaissance(new Date())
+                        .build()
+        );
+        patientRepo.save(
+                Patient.builder()
+                        .nom("Maria")
+                        .malade(Math.random()>0.5)
+                        .score((int) Math.random() * 100)
+                        .dateNaissance(new Date())
+                        .build()
+        );
+        patientRepo.save(
+                Patient.builder()
+                        .nom("Doe")
+                        .malade(Math.random()>0.5)
+                        .score((int) Math.random() * 100)
+                        .dateNaissance(new Date())
+                        .build()
+        );
+        patientRepo.save(
+                Patient.builder()
+                        .nom("Cartner")
+                        .malade(Math.random()>0.5)
+                        .score((int) Math.random() * 100)
+                        .dateNaissance(new Date())
+                        .build()
+        );
+        patientRepo.save(
+                Patient.builder()
+                        .nom("Doe")
+                        .malade(Math.random()>0.5)
+                        .score((int) Math.random() * 100)
+                        .dateNaissance(new Date())
+                        .build()
+        );
+        patientRepo.save(
+                Patient.builder()
+                        .nom("Cartner")
+                        .malade(Math.random()>0.5)
+                        .score((int) Math.random() * 100)
+                        .dateNaissance(new Date())
+                        .build()
+        );
+        patientRepo.save(
+                Patient.builder()
+                        .nom("Doe")
+                        .malade(Math.random()>0.5)
+                        .score((int) Math.random() * 100)
+                        .dateNaissance(new Date())
+                        .build()
+        );
+        patientRepo.save(
+                Patient.builder()
+                        .nom("Cartner")
+                        .malade(Math.random()>0.5)
+                        .score((int) Math.random() * 100)
+                        .dateNaissance(new Date())
+                        .build()
+        );
+
+    }
 }
