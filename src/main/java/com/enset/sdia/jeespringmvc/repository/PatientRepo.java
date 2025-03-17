f5f3cbdadd98634ceb51207b5294cc55e4b3b2b5
walidmoufadil <walid.moufadil-etu@etu.univh2c.ma>
2025-03-17 16:16:38 +0000

    add search by name method

--- src/main/java/com/enset/sdia/jeespringmvc/repository/PatientRepo.java
+++ src/main/java/com/enset/sdia/jeespringmvc/repository/PatientRepo.java
@@ -1,10 +1,12 @@
 package com.enset.sdia.jeespringmvc.repository;
 
 import com.enset.sdia.jeespringmvc.entity.Patient;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PatientRepo extends JpaRepository<Patient,Long> {
-
+    Page<Patient> findByNomContains(String nom, Pageable pageable);
 }
