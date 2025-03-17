1934543b4604a869c2ef44a3295ae21b5cc81366
walidmoufadil <walid.moufadil-etu@etu.univh2c.ma>
2025-03-17 13:49:48 +0000

    add Patient Repository

--- src/main/java/com/enset/sdia/jeespringmvc/repository/PatientRepo.java
+++ src/main/java/com/enset/sdia/jeespringmvc/repository/PatientRepo.java
@@ -1,2 +1,10 @@
-package com.enset.sdia.jeespringmvc.repository;public interface PatientRepo {
+package com.enset.sdia.jeespringmvc.repository;
+
+import com.enset.sdia.jeespringmvc.entity.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PatientRepo extends JpaRepository<Patient,Long> {
+
 }
