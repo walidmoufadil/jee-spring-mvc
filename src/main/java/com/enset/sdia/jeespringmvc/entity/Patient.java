326a765b6561f74de82ca9466245b050cc073ecc
walidmoufadil <walid.moufadil-etu@etu.univh2c.ma>
2025-03-24 23:34:00 +0000

    edit Patient entity

--- src/main/java/com/enset/sdia/jeespringmvc/entity/Patient.java
+++ src/main/java/com/enset/sdia/jeespringmvc/entity/Patient.java
@@ -1,13 +1,13 @@
 package com.enset.sdia.jeespringmvc.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
 
@@ -16,7 +16,10 @@ import java.util.Date;
 public class Patient {
     @Id@GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotEmpty
     private String nom;
+    @Temporal(TemporalType.DATE)
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date dateNaissance;
     private boolean malade;
     private int score;
