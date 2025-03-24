812bbffe7bf83b2df3ef27d06aecc399aa9eda8c
walidmoufadil <walid.moufadil-etu@etu.univh2c.ma>
2025-03-24 23:33:36 +0000

    edit Patient Controller

--- src/main/java/com/enset/sdia/jeespringmvc/web/PatientController.java
+++ src/main/java/com/enset/sdia/jeespringmvc/web/PatientController.java
@@ -2,12 +2,15 @@ package com.enset.sdia.jeespringmvc.web;
 
 import com.enset.sdia.jeespringmvc.entity.Patient;
 import com.enset.sdia.jeespringmvc.repository.PatientRepo;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -39,9 +42,26 @@ public class PatientController {
         return "redirect:/patient/index";
     }
 
-    @GetMapping("formPatient")
+    @GetMapping("/formPatient")
     public String formPatient(Model model){
         model.addAttribute("patient", new Patient());
         return "formPatient";
+    }
+    @PostMapping("/save")
+    public String save(Model model, @Valid Patient patient, BindingResult bindingResult, @RequestParam(defaultValue = "0")int page, @RequestParam(defaultValue = "") String keyword){
+        if(bindingResult.hasErrors()){
+            return "redirect:/patient/formPatient";
+        }
+        patientRepo.save(patient);
+        return "redirect:/patient/index?page="+page+"&keyword="+keyword;
+    }
+
+    @GetMapping("/editPatient")
+    public String editPatient(Model model, Long id,String keyword, int page){
+        Patient patient = patientRepo.findById(id).orElseThrow(()->new RuntimeException("Patient not found"));
+        model.addAttribute("patient",patient);
+        model.addAttribute("page",page);
+        model.addAttribute("keyword",keyword);
+        return "editPatient";
     }
 }
